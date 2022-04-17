179633d1b028f255210785f1d5afdf4643ee20eb
Faizan <baig.faizan1999@gmail.com>
2022-04-18 03:27:47 +0530

    Updates

---
+++ precache-manifest.42bd2abddac877c4cf58a37ba3882a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0796296034cc319494d4fa2b11be9fd6",
+    "url": "/fetch-movies-app/index.html"
+  },
+  {
+    "revision": "44135949f944c146e25e",
+    "url": "/fetch-movies-app/static/css/main.5039e374.chunk.css"
+  },
+  {
+    "revision": "76f954d35eaea4e1a4cd",
+    "url": "/fetch-movies-app/static/js/2.2a638db3.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/fetch-movies-app/static/js/2.2a638db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44135949f944c146e25e",
+    "url": "/fetch-movies-app/static/js/main.29ad7591.chunk.js"
+  },
+  {
+    "revision": "5fd59133b23966471956",
+    "url": "/fetch-movies-app/static/js/runtime-main.b192e137.js"
+  }
+]);
